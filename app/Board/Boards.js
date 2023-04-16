e138c6b4f92fbd2f2ab6f7b717920076f43f1a06
Fede <fnotalvares@gmail.com>
2023-04-16 12:54:43 +0200

    Test generateCells method on Board class

--- app/Board/Boards.js
+++ app/Board/Boards.js
@@ -1,22 +1,23 @@
 import { Cells } from "../Cell/Cell.js";
 
 class Board {
-  constructor(totalRows, totalColumns) {
-    this.totalRows = totalRows;
-    this.totalColumns = totalColumns;
-    this.cells = this.createCells;
-    const cells = [];
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.cells = this.generateCells;
   }
 
-  createCells() {
-    for (let rows = 0; rows < totalRows; rows++) {
+  generateCells(x, y) {
+    const cells = [];
+    for (let rows = 0; rows < this.height; rows++) {
       const row = [];
-
-      for (let columnIndex = 0; columnIndex < totalColumns; columnIndex++) {
+      for (let cells = 0; cells < this.width; cells++) {
         row.push(new Cell(x, y, Math.random() < 0.5));
       }
+
       cells.push(row);
     }
+
     return cells;
   }
 }
---
+++ app/Board/Boards.test.js
@@ -0,0 +1,25 @@
+import Board from "./Boards";
+
+describe("Given a method generateCells,", () => {
+  describe("when it receives a width of 2 and a height of 2,", () => {
+    test("then it should return an array which includes two arrays with two cell instances containing x, y and alive properties each one", () => {
+      const width = 2;
+      const height = 2;
+      const expectedResult = [
+        [
+          { x: undefined, y: undefined, alive: true },
+          { x: undefined, y: undefined, alive: true },
+        ],
+        [
+          { x: undefined, y: undefined, alive: true },
+          { x: undefined, y: undefined, alive: true },
+        ],
+      ];
+      const board = new Board(width, height);
+
+      const cells = board.generateCells();
+
+      expect(cells).toEqual(expectedResult);
+    });
+  });
+});
